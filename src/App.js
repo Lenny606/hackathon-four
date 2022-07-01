6246e59a064c47f0e5563826419e341e76891df5
Tomáš Kravčík <thomas.kravcik@gmail.com>
2022-07-01 09:37:02 +0200

    base project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './Components/NavBar';
+import TopFlights from './Components/TopFlights';
+import { useState, useEffect } from 'react';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
 function App() {
+ 
+  const [state, setState] = useState([]);
+  const [loading, setLoading] = useState(false)
+ 
+  const url = '';
+
+  const loadData = async () => {
+
+    try {
+
+      setLoading(true);
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data.message);
+
+
+      //read the data and access them accordingly if one object or array etc...
+      //console.log(data[0].name);
+      setState(data);
+    }
+    catch (err) {
+
+    }
+    finally {
+
+      setLoading(false);
+
+    }
+
+  }
+ 
+  useEffect(() => {
+    loadData();
+    //add dependancy variable to call function after re-render    
+  }, []);
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <BrowserRouter>
+      <nav className='navigation' >
+        <NavBar />
+      </nav>
+
+      <Routes>
+        <Route path='/topflights' element={<TopFlights loading={loading} data={state} />} />
+        {/* <Route path='/search/:name' element={<Search />} /> */}
+      </Routes>
+
+
+
+    </BrowserRouter>
+  </>
   );
 }
 
